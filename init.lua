42f63d8bde2e5d02dac5ffff2054897d3d834bd6
xiantang <zhujingdi1998@gmail.com>
2023-05-09 21:21:29 +0800

    init project

---
+++ init.lua
@@ -0,0 +1,24 @@
+local set = vim.o
+set.number = true
+set.relativenumber = true
+set.clipboard = "unnamed"
+
+-- 在 copy 后高亮
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	pattern = { "*" },
+	callback = function()
+		vim.highlight.on_yank({
+			timeout = 300,
+		})
+	end,
+})
+
+
+
+-- keybindings
+local opt = { noremap = true, silent = true }
+
+vim.keymap.set("n", "<C-l>", "<C-w>l", opt)
+vim.keymap.set("n", "<C-h>", "<C-w>h", opt)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opt)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opt)
