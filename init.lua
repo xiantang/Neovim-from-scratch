b43bd4280740e4377bc1b51078504680417162ae
xiantang <zhujingdi1998@gmail.com>
2023-05-14 16:11:52 +0800

    restore

--- init.lua
+++ init.lua
@@ -111,7 +111,7 @@ require("lazy").setup({
 			{ "<leader>p", ":Telescope find_files<CR>", desc = "find files" },
 			{ "<leader>P", ":Telescope live_grep<CR>", desc = "grep file" },
 			{ "<leader>rs", ":Telescope resume<CR>", desc = "resume" },
-			{ "<leader>q", ":Telescope oldfiles<CR>", desc = "oldfiles" },
+			{ "<C-q>", ":Telescope oldfiles<CR>", desc = "oldfiles" },
 		},
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.1",
