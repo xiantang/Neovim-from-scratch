79e72d3e87fd935ae9bf21b1cbe45ae67bd58163
jingdi.zhu <jing.zhu@shopee.com>
2023-05-23 22:02:02 +0800

    Add echo function to example.py and set up dap and dapui for debugging in init.lua. Update nvim-dap and nvim-treesitter-textobjects in lazy-lock.json.

---
+++ example.py
@@ -0,0 +1,13 @@
+def echo(input):
+    print(input)
+    a = 1
+    b = 2
+    c = a + b
+    print(c)
+
+
+a = 1
+b = 2
+c = a + b
+echo(c)
+print(111)
--- init.lua
+++ init.lua
@@ -41,6 +41,12 @@ end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	{
+		"mfussenegger/nvim-dap",
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+	},
+	{
 		"folke/persistence.nvim",
 		event = "BufReadPre", -- this will only start session saving when an actual file was opened
 		config = function()
@@ -450,3 +456,54 @@ end
 vim.api.nvim_set_hl(0, "@lsp.type.variable.lua", { link = "Normal" })
 vim.api.nvim_set_hl(0, "Identifier", { link = "Normal" })
 vim.api.nvim_set_hl(0, "TSVariable", { link = "Normal" })
+
+-- dap set up
+local dap, dapui = require("dap"), require("dapui")
+dap.adapters.python = function(cb, config)
+	if config.request == "attach" then
+		---@diagnostic disable-next-line: undefined-field
+		local port = (config.connect or config).port
+		---@diagnostic disable-next-line: undefined-field
+		local host = (config.connect or config).host or "127.0.0.1"
+		cb({
+			type = "server",
+			port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+			host = host,
+			options = {
+				source_filetype = "python",
+			},
+		})
+	else
+		cb({
+			type = "executable",
+			command = "/usr/local/bin/python",
+			args = { "-m", "debugpy.adapter" },
+			options = {
+				source_filetype = "python",
+			},
+		})
+	end
+end
+
+dap.configurations.python = {
+	{
+		type = "python",
+		request = "launch",
+		name = "Launch file",
+		program = "${file}",
+		pythonPath = function()
+			return "/usr/local/bin/python"
+		end,
+	},
+}
+
+require("dapui").setup()
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dapui.close()
+end
